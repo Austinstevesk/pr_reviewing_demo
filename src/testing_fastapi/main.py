9b3e6f76cb56aea57ed7fd6d1933c5ffe33d597b
Austinstevesk <austinstevesk@gmail.com>
2025-04-29 21:06:18 +0300

    fixed the changes

--- src/testing_fastapi/main.py
+++ src/testing_fastapi/main.py
@@ -1,19 +1,19 @@
 from typing import List
 
-
 from fastapi import FastAPI, HTTPException, status
-
 
 app = FastAPI()
 
 items = {"foo": {"name": "Foo", "description": "A test item"}}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: str):
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item not found")
     return items[item_id]
-    
+
+
 @app.post("/items/{item_id}", status_code=status.HTTP_201_CREATED)
 async def create_item(item_id: str, item: dict):
     if item_id in items:
@@ -21,21 +21,12 @@ async def create_item(item_id: str, item: dict):
     items[item_id] = item
     return item
 
-@app.post("/items/{item_id}", status_code=status.HTTP_201_CREATED)
-async def create_item(item_id: str, item: dict):
-    if item_id in items:
-        raise HTTPException(status_code=400, detail="Item already exists")
-    items[item_id] = item
-    return item
 
-
-@app.post("/list_of_things/{item_id}", status_code=status.HTTP_201_CREATED, response_model=List[dict], description="Get a list of items")
-async def get_a_list_of_items():
-    return [
-        {
-            "username": "random user_username",
-            "data": [
-                "user data here"
-            ]
-        }
-    ]
+@app.get(
+    "/list-of-users",
+    status_code=status.HTTP_201_CREATED,
+    response_model=List[dict],
+    description="Get a list of user data that we have on the database",
+)
+async def get_a_list_of_users():
+    return [{"username": "random user_username", "data": ["user data here"]}]
