2c21f7f5b2172f80951bb180bc634049cccb21f4
Austinstevesk <austinstevesk@gmail.com>
2025-04-29 20:20:43 +0300

    update crud

--- src/testing_fastapi/crud.py
+++ src/testing_fastapi/crud.py
@@ -2,9 +2,6 @@ def get_item(collection, key):
     return collection[key]
 
 def create_item(collection, key, value):
-
-
-    
     if key in collection:
         raise ValueError("Key already exists")
     collection[key] = value
