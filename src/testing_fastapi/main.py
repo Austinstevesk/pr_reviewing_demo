ae95f5a7623d6ae74af0df6970d2eb455a991d9b
Austinstevesk <austinstevesk@gmail.com>
2025-04-29 20:37:12 +0300

    create a new endpoint

--- src/testing_fastapi/main.py
+++ src/testing_fastapi/main.py
@@ -1,4 +1,8 @@
+from typing import List
+
+
 from fastapi import FastAPI, HTTPException, status
+
 
 app = FastAPI()
 
@@ -9,6 +13,13 @@ async def read_item(item_id: str):
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item not found")
     return items[item_id]
+    
+@app.post("/items/{item_id}", status_code=status.HTTP_201_CREATED)
+async def create_item(item_id: str, item: dict):
+    if item_id in items:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    items[item_id] = item
+    return item
 
 @app.post("/items/{item_id}", status_code=status.HTTP_201_CREATED)
 async def create_item(item_id: str, item: dict):
@@ -16,3 +27,15 @@ async def create_item(item_id: str, item: dict):
         raise HTTPException(status_code=400, detail="Item already exists")
     items[item_id] = item
     return item
+
+
+@app.post("/list_of_things/{item_id}", status_code=status.HTTP_201_CREATED, response_model=List[dict], description="Get a list of items")
+async def get_a_list_of_items():
+    return [
+        {
+            "username": "random user_username",
+            "data": [
+                "user data here"
+            ]
+        }
+    ]
